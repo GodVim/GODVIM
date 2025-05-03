c63a524dfd70126524c980f584459f131ec1b532
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-03 10:36:59 -0700

    banannas

--- lua/godvim/plugins/lsp/lspconfig.lua
+++ lua/godvim/plugins/lsp/lspconfig.lua
@@ -1,10 +1,13 @@
 return {
 	'neovim/nvim-lspconfig',
-	event = "BufReadPost",
+	event = "InsertEnter",
 	dependencies = {
 		'zeioth/garbage-day.nvim',
 		'williamboman/mason.nvim',
-		'nvim-java/nvim-java',
+		{
+			'nvim-java/nvim-java',
+			event = "InsertEnter"
+		},
 		'williamboman/mason-lspconfig.nvim',
 	},
 	config = function()
@@ -31,6 +34,15 @@ return {
 
 		require('java').setup()
 
+		lspconfig[lua_ls].setup({
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { 'vim' }, -- Tell lua_ls that `vim` is a global
+					},
+				},
+			},
+		})
 		for _, server in ipairs(servers) do
 			lspconfig[server].setup({})
 		end
