d90c4cd13b92c6cd05c9581231e6ca97e4890051
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-12 20:20:04 -0700

    asdfasdfEJFIDSOPFSD OFIJD ISD

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -12,7 +12,7 @@ opt.smartcase = true
 
 opt.updatetime = 200
 opt.clipboard:append("unnamedplus")
-
+opt.statuscolumn = "%{%v:lua.custom_statuscol()%}"
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
--- lua/godvim/plugins/ui/status.lua
+++ lua/godvim/plugins/ui/status.lua
@@ -1,37 +1,48 @@
 return {
-  "rebelot/heirline.nvim",
-  event = "UiEnter",
-  dependencies = { "Zeioth/heirline-components.nvim" },
-  opts = function()
-    local lib = require("heirline-components.all")
+  {
+    "rebelot/heirline.nvim",
+    event = "UiEnter",
+    dependencies = { "Zeioth/heirline-components.nvim" },
+    opts = function()
+      local lib = require("heirline-components.all")
 
-    return {
-      statusline = {
-        hl = { fg = "fg", bg = "bg" },
-        lib.component.mode(),
-        lib.component.git_branch(),
-        lib.component.file_info(),
-        lib.component.breadcrumbs(),
-        lib.component.git_diff(),
-        lib.component.diagnostics(),
-        lib.component.fill(),
-        lib.component.cmd_info(),
-        lib.component.fill(),
-        lib.component.lsp(),
-        lib.component.compiler_state(),
-        lib.component.virtual_env(),
-        lib.component.nav(),
-      },
-    }
-  end,
-  config = function(_, opts)
-    vim.o.cmdheight = 0
-    local heirline = require("heirline")
-    local lib = require("heirline-components.all")
+      return {
+        tabline = { -- UI upper bar
+          lib.component.tabline_conditional_padding(),
+          lib.component.tabline_buffers(),
+          lib.component.fill { hl = { bg = "tabline_bg" } },
+          lib.component.tabline_tabpages()
+        },
+        statusline = {
+          hl = { fg = "fg", bg = "bg" },
+          lib.component.mode(),
+          lib.component.file_info(),
+          lib.component.git_diff(),
+          lib.component.diagnostics(),
+          lib.component.fill(),
+          lib.component.cmd_info(),
+          lib.component.fill(),
+          lib.component.lsp(),
+          lib.component.compiler_state(),
+          lib.component.virtual_env(),
+          lib.component.nav(),
+        },
+        statuscolumn = {
+          lib.component.foldcolumn(),
+          lib.component.fill(),
+          lib.component.numbercolumn(),
+          lib.component.signcolumn(),
+        },
+      }
+    end,
+    config = function(_, opts)
+      vim.o.cmdheight = 0
+      local heirline = require("heirline")
+      local lib = require("heirline-components.all")
 
-    -- Setup
-    lib.init.subscribe_to_events()
-    heirline.load_colors(lib.hl.get_colors())
-    heirline.setup(opts)
-  end,
-}
+      -- Setup
+      lib.init.subscribe_to_events()
+      heirline.load_colors(lib.hl.get_colors())
+      heirline.setup(opts)
+    end,
+  } }
