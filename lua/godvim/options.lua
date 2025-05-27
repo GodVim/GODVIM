8cd8078b73071e7331bdb8d022d8544e26778835
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-27 16:57:04 -0700

    Update options.lua

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -12,7 +12,6 @@ opt.smartcase = true
 
 opt.updatetime = 200
 opt.clipboard:append("unnamedplus")
-opt.statuscolumn = "%{%v:lua.custom_statuscol()%}"
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
