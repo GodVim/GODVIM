b54e825ba494ad118a5c4aee95fd52a666a9cc6b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-27 16:52:08 -0700

    Update lspconfig.lua

--- lua/godvim/plugins/lang/lspconfig.lua
+++ lua/godvim/plugins/lang/lspconfig.lua
@@ -91,40 +91,21 @@ return {
       if vim.fn.has("nvim-0.10") == 1 then
         -- inlay hints
         if opts.inlay_hints.enabled then
-          LazyVim.lsp.on_supports_method("textDocument/inlayHint", function(client, buffer)
-            if
-              vim.api.nvim_buf_is_valid(buffer)
-              and vim.bo[buffer].buftype == ""
-              and not vim.tbl_contains(opts.inlay_hints.exclude, vim.bo[buffer].filetype)
-            then
+  
               vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
-            end
           end)
         end
 
         -- code lens
         if opts.codelens.enabled and vim.lsp.codelens then
-          LazyVim.lsp.on_supports_method("textDocument/codeLens", function(client, buffer)
             vim.lsp.codelens.refresh()
             vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
               buffer = buffer,
               callback = vim.lsp.codelens.refresh,
             })
-          end)
         end
       end
 
-      if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
-        opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
-          or function(diagnostic)
-            local icons = LazyVim.config.icons.diagnostics
-            for d, icon in pairs(icons) do
-              if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
-                return icon
-              end
-            end
-          end
-      end
 
       vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
 
