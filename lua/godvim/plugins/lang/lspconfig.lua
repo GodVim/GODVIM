49b55eeb6bb051a52dca3353804e8da23bdbd6ed
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-27 16:51:21 -0700

    Update lspconfig.lua

--- lua/godvim/plugins/lang/lspconfig.lua
+++ lua/godvim/plugins/lang/lspconfig.lua
@@ -53,14 +53,6 @@ return {
             -- prefix = "icons",
           },
           severity_sort = true,
-          signs = {
-            text = {
-              [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
-              [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
-              [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
-              [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
-            },
-          },
         },
       },
     config = function()
