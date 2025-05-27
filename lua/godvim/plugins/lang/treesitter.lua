36e0b08466ff53fe4a1dc22e3a2fcea4f101d68b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-27 13:38:19 -0700

    Update treesitter.lua

--- lua/godvim/plugins/lang/treesitter.lua
+++ lua/godvim/plugins/lang/treesitter.lua
@@ -9,12 +9,27 @@ return {
     }
   },
   {
+    "windwp/nvim-ts-autotag",
+    ft = { "astro", "html", "javascript", "jsx", "markdown", "svelte", "tsx", "typescript", "xml" },
+    opts = {
+      -- Defaults
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = false -- Auto close on trailing </
+    },
+    -- Also override individual filetype configs, these take priority.
+    -- Empty by default, useful if one of the "opts" global settings
+    -- doesn't work well in a specific filetype
+    per_filetype = {
+      ["html"] = {
+        enable_close = false
+      }
+    }
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
-    dependencies = {
-      "windwp/nvim-ts-autotag",
-    },
     opts = {
       highlight = {
         enable = true,
