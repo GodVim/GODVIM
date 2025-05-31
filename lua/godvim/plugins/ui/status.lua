eef9f871f00b2f6cf67173ff8b1efcafb77e1951
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-31 11:18:27 -0700

    Update status.lua

--- lua/godvim/plugins/ui/status.lua
+++ lua/godvim/plugins/ui/status.lua
@@ -1,13 +1,37 @@
 return {
   {
-    "folke/snacks.nvim",
-    event = "UiEnter",
-    ---@type snacks.Config
-    opts = {
-      statuscolumn = {
-        enabled = true,
-      }
-    }
+    "luukvbaal/statuscol.nvim",
+    event="UiEnter",
+    opts = function()
+      local builtin = require("statuscol.builtin")
+      return {
+          relculright = true,
+  segments = {
+    {
+      text = { builtin.foldfunc },
+      click = "v:lua.ScFa"
+    },
+    {
+      -- Segment for Diagnostic signs (e.g., from LSP)
+      sign = { name = { "Diagnostic" } }, -- Only show signs named "Diagnostic"
+      maxwidth = 2,                       -- Segment max width
+      auto = true,                        -- Collapse segment if no diagnostic signs
+      click = "v:lua.ScSa"
+    },
+    {
+      text = { builtin.lnumfunc },
+      click = "v:lua.ScLa"
+    },
+    {
+      -- Segment for other signs (e.g., gitsigns, bookmarks)
+      sign = { name = { ".*" } },         -- Match any other sign name (regex)
+      maxwidth = 2,                       -- Segment max width
+      colwidth = 1,                       -- Hint for sign column width within this segment
+      auto = true,                        -- Collapse segment if no other signs
+      click = "v:lua.ScSa"                -- Note: same click handler as Diagnostic signs
+    },
+  }
+      end,
   },
   {
     "rebelot/heirline.nvim",
