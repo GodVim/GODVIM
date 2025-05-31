b5f8d0013a8fe729370a913bb37f6d52f07bb879
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-31 11:11:35 -0700

    Update options.lua

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -2,14 +2,11 @@ vim.loader.enable()
 local opt = vim.opt
 local o = vim.o
 
---o.foldcolumn = '1'
---o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
---o.foldlevelstart = 99
---o.foldenable = true
+o.foldcolumn = '1'
+o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+o.foldlevelstart = 99
+o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-vim.o.foldcolumn = '1'
-
-vim.wo.statuscolumn = "%f\ [%l,%c]"
 
 opt.tabstop = 2
 opt.shiftwidth = 2
