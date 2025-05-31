e328f586c5a7c9d4e6c54de5b56f62b7c5103d9f
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-31 10:40:58 -0700

    Update folding.lua

--- lua/godvim/plugins/lang/folding.lua
+++ lua/godvim/plugins/lang/folding.lua
@@ -2,6 +2,12 @@ return {
   'kevinhwang91/nvim-ufo',
   event = "BufReadPre",
   cmd = {"UfoEnable"},
-  opts = {},
+  config = function()
+    require('ufo').setup({
+      provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+      end
+    })
+  end,
   dependencies = {'kevinhwang91/promise-async'},
 }
