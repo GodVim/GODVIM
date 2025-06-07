2b1edd1781ce247ec7c090cbffa9d406a688143b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-07 13:45:19 -0700

    Update ai.lua

--- lua/godvim/plugins/ai.lua
+++ lua/godvim/plugins/ai.lua
@@ -8,6 +8,16 @@ return {
       },
       inline = {
         adapter = "copilot",
+               keymaps = {
+        accept_change = {
+          modes = { n = "ga" },
+          description = "Accept the suggested change",
+        },
+        reject_change = {
+          modes = { n = "gr" },
+          description = "Reject the suggested change",
+        },
+      },
       },
     },
     adapters = {
