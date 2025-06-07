eccb6c02affc0ec72019d9f2de0c4e8c207b2b5c
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-07 13:43:01 -0700

    Update options.lua

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -27,4 +27,3 @@ opt.clipboard:append("unnamedplus")
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append "<>[]hl"
-display.action_palette.provider = "telescope"
