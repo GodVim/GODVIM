1d9e85eaf843b309a225817168faac8cfbd16f28
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-08 10:02:06 -0700

    Update lspconfig.lua

--- lua/godvim/plugins/lang/lspconfig.lua
+++ lua/godvim/plugins/lang/lspconfig.lua
@@ -1,61 +1,62 @@
 return {
-  { "mason-org/mason.nvim", lazy = true, version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", lazy = true, version = "^1.0.0" },
-  { "nvim-java/nvim-java", lazy = true, ft="java",config=function()require('java').setup({
-  jdk = {
-    auto_install = false,
-  },
-})
-      lspconfig.jdtls.setup({})
-    end,
-    },
   {
-    "neovim/nvim-lspconfig",
+    "mason-org/mason.nvim",
     event = "BufReadPre",
-    dependencies = {
-      "zeioth/garbage-day.nvim",
-      {
-        "mason-org/mason.nvim",
-         --version = "^1.0.0",
-        opts = {
-          registries = {
-            'github:nvim-java/mason-registry@2024-12-24-graceful-raft',
-            'github:mason-org/mason-registry@2025-05-10-new-sprout',
-          },
-        },
-      },
-
-    },
-
+    version = "^1.0.0",
     config = function()
-      local mason = require("mason")
-      local lspconfig = require("lspconfig")
+        local mason = require("mason")
 
-      mason.setup()
+        mason.setup({
+          registries = {
+            "github:nvim-java/mason-registry@2024-12-24-graceful-raft",
+            "github:mason-org/mason-registry@2025-05-10-new-sprout"
+          }
+        })
 
-      local servers = {
-        "lua_ls",
-        --"ruff",
-        "jsonls",
-        "jdtls",
-        "yamlls",
-        "pyright",
-        "marksman",
-      }
+        local servers = {
+          "lua_ls",
+          --"ruff",
+          "jsonls",
+          "jdtls",
+          "yamlls",
+          "pyright",
+          "marksman"
+        }
 
-
-      require("mason-lspconfig").setup({
-        ensure_installed = servers,
-      })
-
-
-
-      lspconfig.lua_ls.setup({})
-      lspconfig.jsonls.setup({})
-     -- lspconfig.ruff.setup({})
-      lspconfig.yamlls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.marksman.setup({})
+        require("mason-lspconfig").setup({
+          ensure_installed = servers
+        })
     end,
-  }
+  },
+  {"mason-org/mason-lspconfig.nvim", lazy = true, version = "^1.0.0"},
+  {
+    "nvim-java/nvim-java",
+    lazy = true,
+    ft = "java",
+    config = function()
+      require("java").setup({
+        jdk = {
+          auto_install = false
+        }
+      })
+      lspconfig.jdtls.setup({})
+    end
+  },
+    {
+        "neovim/nvim-lspconfig",
+        event = "BufReadPre",
+        dependencies = {
+            "zeioth/garbage-day.nvim"
+        },
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.lua_ls.setup({})
+            lspconfig.jsonls.setup({})
+            -- lspconfig.ruff.setup({})
+            lspconfig.yamlls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.marksman.setup({})
+        end
+    }
 }
