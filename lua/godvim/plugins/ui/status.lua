4c63c01be1ac5605422ea0bd6683bcce837e0e63
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-08 14:27:12 -0700

    Update status.lua

--- lua/godvim/plugins/ui/status.lua
+++ lua/godvim/plugins/ui/status.lua
@@ -27,40 +27,7 @@ return {
     priority = "1",
     dependencies = {
       "Zeioth/heirline-components.nvim",
-      {
-  "lmantw/themify.nvim",
-  event ="VimEnter",
-  opts = {
-    {
-      "folke/tokyonight.nvim",
-      blacklist = { "tokyonight-day", "tokyonight-storm" },
-    },
-    {
-      "everviolet/nvim",
-      branch = "mega",
-      blacklist = { "evergarden-winter", "evergarden-spring" },
-    },
-    {
-      "catppuccin/nvim",
-      blacklist = { "catppuccin-latte", "catppuccin-mocha" },
-    },
-    {
-      "rose-pine/neovim",
-      blacklist = { "rose-pine-dawn", "rose-pine-moon" },
-    },
-    {
-      "rmehri01/onenord.nvim",
-      branch = "main",
-      blacklist = { "onenord-light" },
-    },
-    "navarasu/onedark.nvim",
-    "gbprod/nord.nvim",
-    "EdenEast/nightfox.nvim",
-    "sainnhe/sonokai",
-    "loctvl842/monokai-pro.nvim",
-    "default"
-  }
-}
+  "lmantw/themify.nvim"
 
     },
     opts = function()
