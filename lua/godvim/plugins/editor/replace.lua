3fc95f86cdf21846c8bfa1471c04d47b63aedd23
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-08 09:41:30 -0700

    Update replace.lua

--- lua/godvim/plugins/editor/replace.lua
+++ lua/godvim/plugins/editor/replace.lua
@@ -1,25 +1,6 @@
 return {
   {
-    "cshuaimin/ssr.nvim",
-    lazy = true,
-    module = "ssr",
-    opts = {
-      border = "rounded",
-      min_width = 50,
-      min_height = 5,
-      max_width = 120,
-      max_height = 25,
-      adjust_window = true,
-      keymaps = {
-        close = "q",
-        next_match = "n",
-        prev_match = "N",
-        replace_confirm = "<cr>",
-        replace_all = "<leader><cr>",
-      },
-    },  
-  },
-  {
+    cmd={"GrugFar", "GrugFarWithin"},
     'MagicDuck/grug-far.nvim',
     -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
     -- additional lazy config to defer loading is not really needed...
