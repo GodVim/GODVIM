be6ea38766bb1eb49591c71ce3f3d6b9b50ffd96
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-12 12:51:53 -0700

    Delete lua/godvim/plugins/projects.lua

--- lua/godvim/plugins/projects.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ahmedkhalf/project.nvim",
-  opts = {},
-  cmd = "Telescope",
-  config = function()
-    require("project_nvim").setup()
-    require('telescope').load_extension('projects')
-  end,
-}
