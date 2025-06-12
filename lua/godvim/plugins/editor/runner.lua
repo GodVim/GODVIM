6a71f0ebf9d28bb615f07568754e65d6c3da08d8
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-12 15:13:26 -0700

    Update runner.lua

--- lua/godvim/plugins/editor/runner.lua
+++ lua/godvim/plugins/editor/runner.lua
@@ -1,7 +1,14 @@
 return {
+{
   "aliqyan-21/runTA.nvim",
   config = function()
     require("runTA.commands").setup()
   end,
   cmd = { "RunCode", "ReopenLastOutput", "Output" },
+},
+{
+  'stevearc/overseer.nvim',
+  opts = {},
+  cmd = { "OverseerRun", "OverseerToggle" }
+}
 }
