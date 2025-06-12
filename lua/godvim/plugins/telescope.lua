f949c2bf93083b5c05aed1a683ef601bc32b6298
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-12 15:25:45 -0700

    Update telescope.lua

--- lua/godvim/plugins/telescope.lua
+++ lua/godvim/plugins/telescope.lua
@@ -1,4 +1,21 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  cmd = "Telescope",
+  {
+    'nvim-telescope/telescope.nvim',
+    cmd = "Telescope",
+    opts = {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+          }
+        }
+      }
+    }
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    event = "BufReadPost",
+    config = function()
+      require("telescope").load_extension("ui-select")
+    end,
+  }
 }
