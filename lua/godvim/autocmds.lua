e4f325c3f1208eaffc42fab3fcd1833c5a7ab5a2
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-13 08:31:53 -0700

    Update autocmds.lua

--- lua/godvim/autocmds.lua
+++ lua/godvim/autocmds.lua
@@ -3,13 +3,57 @@ local autocmd = vim.api.nvim_create_autocmd
 local cmd = vim.api.nvim_create_user_command
 vim.deprecate = function() end
 
+function run_cmd(cmd, show_error)
+  -- Split cmd string into a list, if needed.
+  if type(cmd) == "string" then
+    cmd = vim.split(cmd, " ")
+  end
+
+  -- If windows, and prepend cmd.exe
+  if vim.fn.has("win32") == 1 then
+    cmd = vim.list_extend({ "cmd.exe", "/C" }, cmd)
+  end
+
+  -- Execute cmd and store result (output or error message)
+  local result = vim.fn.system(cmd)
+  local success = vim.api.nvim_get_vvar("shell_error") == 0
+
+  -- If the command failed and show_error is true or not provided, print error.
+  if not success and (show_error == nil or show_error) then
+    vim.api.nvim_echo({{
+      ("Error running command %s\nError message:\n%s"):format(
+        table.concat(cmd, " "), -- Convert the cmd back to string.
+        result                  -- Show the error result
+      )}}, true, { err = true }
+    )
+  end
+
+  -- strip out terminal escape sequences and control characters.
+  local cleaned_result = result:gsub("[\27\155][][()#;?%d]*[A-PRZcf-ntqry=><~]", "")
+
+  -- Return the cleaned result if the command succeeded, or nil if it failed
+  return (success and cleaned_result) or nil
+end
+
+function trigger_event(event, is_urgent)
+  -- define behavior
+  local function trigger()
+    local is_user_event = string.match(event, "^User ") ~= nil
+    if is_user_event then
+      event = event:gsub("^User ", "")
+      vim.api.nvim_exec_autocmds("User", { pattern = event, modeline = false })
+    else
+      vim.api.nvim_exec_autocmds(event, { modeline = false })
+    end
+  end
+
 -- STOLEN FROM NORMALNVIM
 autocmd({ "BufReadPost", "BufNewFile", "BufWritePost" }, {
   desc = "Nvim user events for file detection (BaseFile and BaseGitFile)",
   callback = function(args)
     local empty_buffer = vim.fn.resolve(vim.fn.expand "%") == ""
     local greeter = vim.api.nvim_get_option_value("filetype", { buf = args.buf }) == "alpha"
-    local git_repo = vim.fn.executable("git") == 1 and utils.run_cmd(
+    local git_repo = vim.fn.executable("git") == 1 and run_cmd(
       { "git", "-C", vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand "%"), ":p:h"), "rev-parse" }, false)
 
     -- For any file exept empty buffer, or the greeter (alpha)
