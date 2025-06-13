310900a4fa1ad736006395e989dc19bd3ad784b1
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-12 21:35:02 -0700

    Create lint.lua

---
+++ lua/godvim/plugins/lint.lua
@@ -0,0 +1,13 @@
+return {
+  "nvimdev/guard.nvim",
+  config = function()
+    local ft = require('guard.filetype')
+    ft('typescript,javascript'):fmt('biome'):fmt('prettierd'):lint('eslint')
+    ft('java'):fmt("google-java-format")
+    ft('python'):fmt('isort')
+            :append('black')
+            :lint('mypy')
+            :append('mypyc')
+            :append('dmypy')
+  end,
+}
