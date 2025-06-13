13cf68bf4e5aac57299558e0f67660349c243b53
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-12 20:46:11 -0700

    Update indent.lua

--- lua/godvim/plugins/editor/indent.lua
+++ lua/godvim/plugins/editor/indent.lua
@@ -1,38 +1,5 @@
-if false then return {  "lukas-reineke/indent-blankline.nvim",
-  event = "BufReadPre",
-  opts = {
-    indent = {
-      char = "│",
-      tab_char = "│",
-    },
-    scope = { show_start = false, show_end = false },
-    exclude = {
-      filetypes = {
-        "Trouble",
-        "alpha",
-        "dashboard",
-        "help",
-        "lazy",
-        "mason",
-        "neo-tree",
-        "notify",
-        "snacks_dashboard",
-        "snacks_notif",
-        "snacks_terminal",
-        "snacks_win",
-        "toggleterm",
-        "trouble",
-      },
-    },
-  },
-  main = "ibl",
-}
-end
-
 return {
-  {
-    "nvimdev/indentmini.nvim",
-    opts ={},
-    event = "InsertEnter"
-  }
+  "nvimdev/indentmini.nvim",
+  opts ={},
+  event = "InsertEnter"
 }
