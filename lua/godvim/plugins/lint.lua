51f1799088c113085d23617194ff202c86aecb11
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-12 21:36:14 -0700

    Update lint.lua

--- lua/godvim/plugins/lint.lua
+++ lua/godvim/plugins/lint.lua
@@ -1,13 +1,16 @@
 return {
-  "nvimdev/guard.nvim",
-  config = function()
-    local ft = require('guard.filetype')
-    ft('typescript,javascript'):fmt('biome'):fmt('prettierd'):lint('eslint')
-    ft('java'):fmt("google-java-format")
-    ft('python'):fmt('isort')
-            :append('black')
-            :lint('mypy')
-            :append('mypyc')
-            :append('dmypy')
-  end,
+  {
+    "nvimdev/guard.nvim",
+    config = function()
+      local ft = require('guard.filetype')
+      ft('typescript,javascript'):fmt('biome'):fmt('prettierd'):lint('eslint')
+      ft('java'):fmt("google-java-format")
+      ft('python'):fmt('isort')
+        :append('black')
+        :lint('mypy')
+        :append('mypyc')
+        :append('dmypy')
+    end,
+  },
+  "nvimdev/guard-collection"
 }
