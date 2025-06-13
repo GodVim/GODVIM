1d4badcba5fffe5d897c480b7d326b8ca4aaae7b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-12 21:18:30 -0700

    Update autocmds.lua

--- lua/godvim/autocmds.lua
+++ lua/godvim/autocmds.lua
@@ -1,1 +1,22 @@
 vim.deprecate = function() end
+
+-- STOLEN FROM NORMALNVIM
+autocmd({ "BufReadPost", "BufNewFile", "BufWritePost" }, {
+  desc = "Nvim user events for file detection (BaseFile and BaseGitFile)",
+  callback = function(args)
+    local empty_buffer = vim.fn.resolve(vim.fn.expand "%") == ""
+    local greeter = vim.api.nvim_get_option_value("filetype", { buf = args.buf }) == "alpha"
+    local git_repo = vim.fn.executable("git") == 1 and utils.run_cmd(
+      { "git", "-C", vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand "%"), ":p:h"), "rev-parse" }, false)
+
+    -- For any file exept empty buffer, or the greeter (alpha)
+    if not (empty_buffer or greeter) then
+      utils.trigger_event("User BaseFile")
+
+      -- Is the buffer part of a git repo?
+      if git_repo then
+        utils.trigger_event("User BaseGitFile")
+      end
+    end
+  end,
+})
