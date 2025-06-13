489e105f51ebf4c9a88ba675b9ab312b17df1366
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-13 09:56:32 -0700

    Delete lua/godvim/plugins/editor/replace.lua

--- lua/godvim/plugins/editor/replace.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    cmd={"GrugFar", "GrugFarWithin"},
-    'MagicDuck/grug-far.nvim',
-    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
-    -- additional lazy config to defer loading is not really needed...
-    opts = {},
-  },
-}
