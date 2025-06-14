a09bcfca72f9681b24fd5ebd3165d89b3f82c4ad
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-14 10:49:02 -0700

    Update options.lua

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -23,3 +23,11 @@ opt.clipboard:append("unnamedplus")
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append "<>[]hl"
 vim.opt.history = 50 -- Number of commands to remember in a history table (per buffer).
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
