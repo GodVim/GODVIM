3299d52b75959c5a04cbb97c503352782f081647
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-25 17:25:14 -0700

    Update options.lua

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -15,6 +15,11 @@ opt.smartcase = true
 opt.number = true
 opt.signcolumn = "yes"
 
+if vim.g.neovide then
+    vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+    vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+    vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+end
 
 opt.updatetime = 200
 opt.clipboard:append("unnamedplus")
