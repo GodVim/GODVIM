063dde6677f1051945c74603e2aa1b208906761b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-05 15:43:36 -0700

    Update ai.lua

--- lua/godvim/plugins/editor/ai.lua
+++ lua/godvim/plugins/editor/ai.lua
@@ -5,14 +5,7 @@ return {
   cmd = "Copilot",
   opts = {
     suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>"
-      }
+      enabled = false,
     },
     panel = {
       enabled = false
