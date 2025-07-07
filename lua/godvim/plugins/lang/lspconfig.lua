f1422ef172400cba18a2e914a474dec3c3722274
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-07 11:11:30 -0700

    Update lspconfig.lua

--- lua/godvim/plugins/lang/lspconfig.lua
+++ lua/godvim/plugins/lang/lspconfig.lua
@@ -14,19 +14,11 @@ return {
       "MasonUpdate",
     },
     version = "^1.0.0",
-    opts_extend = { "ensure_installed" },
     opts = {
       registries = {
         "github:nvim-java/mason-registry",
         "github:mason-org/mason-registry",
       },
-      ensure_installed = {
-        "jsonls",
-        "biome",
-        "astro",
-        "prettier",
-        "tailwindcss",
-      }
     }
   },
 
@@ -36,7 +28,13 @@ return {
     version = "^1.0.0",
     opts = {
       automatic_enable = true,
-
+      ensure_installed = {
+        "jsonls",
+        "biome",
+        "astro",
+        "prettier",
+        "tailwindcss",
+      }
     }
   },
 
