9bfbb6b7efa7045c790b4af6f4c39d5c4d5e519f
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-07 12:55:53 -0700

    Update blink.lua

--- lua/godvim/plugins/lang/blink.lua
+++ lua/godvim/plugins/lang/blink.lua
@@ -1,6 +1,25 @@
 return {
   "Saghen/blink.cmp",
   dependencies = {
+    {
+  "zbirenbaum/copilot.lua",
+  event = "InsertEnter",
+  build = ":Copilot auth",
+  cmd = "Copilot",
+  opts = {
+    suggestion = {
+      enabled = false,
+    },
+    panel = {
+      enabled = false
+    },
+    filetypes = {
+      markdown = true,
+      help = true
+    }
+  }
+},
+
     "mikavilpas/blink-ripgrep.nvim",
     "giuxtaposition/blink-cmp-copilot",
   },
