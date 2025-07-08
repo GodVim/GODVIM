b12e68b87a838abb63346f010ca7dd4fa108d7bc
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-07 18:48:40 -0700

    Update autocmds.lua

--- lua/godvim/autocmds.lua
+++ lua/godvim/autocmds.lua
@@ -129,3 +129,10 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     end)
   end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesActionRename",
+  callback = function(event)
+    Snacks.rename.on_rename_file(event.data.from, event.data.to)
+  end,
+})
