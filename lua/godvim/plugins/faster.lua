c3b597ea8c957b697a486c470c766a15bbfaffb7
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-07 18:48:13 -0700

    Update faster.lua

--- lua/godvim/plugins/faster.lua
+++ lua/godvim/plugins/faster.lua
@@ -1,5 +1,13 @@
 return {
-  'pteroctopus/faster.nvim',
-  opts = {},
-  event = "BufReadPre"
+  "folke/snacks.nvim",
+  event = "BufReadPre",
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    scroll = { enabled = true },
+    rename =  { enabled = true },
+  }
 }
