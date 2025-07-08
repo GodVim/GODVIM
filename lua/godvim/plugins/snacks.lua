ae7379c5d4476559f8df0ba83b522e531a05290f
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-07 21:05:14 -0700

    Update snacks.lua

--- lua/godvim/plugins/snacks.lua
+++ lua/godvim/plugins/snacks.lua
@@ -6,8 +6,14 @@ return {
     bigfile = { enabled = true },
     quickfile = { enabled = true },
     indent = { enabled = true },
+    profiler = { enabled = true },
     input = { enabled = true },
     scroll = { enabled = true },
     rename =  { enabled = true },
+  },
+  keys = {
+    { "<leader>pp", "<cmd>lua Snacks.toggle.profiler()<cr>", desc = "Profiler Scratch Bufer" },
+    { "<leader>ph", "<cmd>lua Snacks.toggle.profiler_highlights()<cr>", desc = "Profiler Scratch Bufer" },   
+    { "<leader>ps", "<cmd>lua Snacks.profiler.scratch()<cr>", desc = "Profiler Scratch Bufer" },
   }
 }
