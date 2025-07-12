8f60e8e7fb753f2f30c1c8f2a1e3a544e34b66ef
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-12 11:08:39 -0700

    Update editor.lua

--- lua/godvim/plugins/editor.lua
+++ lua/godvim/plugins/editor.lua
@@ -20,7 +20,7 @@ return {
     "hedyhli/outline.nvim",
     keys = { { "<leader>cs", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
     cmd = "Outline",
-    opts {
+    opts = {
       keymaps = {
         up_and_jump = "<up>",
         down_and_jump = "<down>",
