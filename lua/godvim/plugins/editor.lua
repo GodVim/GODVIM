af8676a04fc5e7a2ac93588a0d834c57f1bbea30
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-12 10:56:02 -0700

    Update editor.lua

--- lua/godvim/plugins/editor.lua
+++ lua/godvim/plugins/editor.lua
@@ -1,47 +1,44 @@
 return {
-	{
-		"folke/snacks.nvim",
-		event = "User FilePost",
-		---@type snacks.Config
-		opts = {
-			indent = { enabled = true },
-			input = { enabled = true },
-		},
-	},
-	{
-		"folke/snacks.nvim",
-		event = "UiEnter",
-		---@type snacks.Config
-		opts = {
-			picker = { enabled = true },
-		},
-	},
-	{
-		"folke/flash.nvim",
-		event = "User FilePost",
-		---@type Flash.Config
-		opts = {},
-    -- stylua: ignore
-    keys = {
-      { "?", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    {
+        "folke/snacks.nvim",
+        event = "User FilePost",
+        ---@type snacks.Config
+        opts = {
+            indent = {enabled = true},
+            input = {enabled = true}
+        }
     },
-	},
-	
-	
-	{
-    "LunarVim/breadcrumbs.nvim",
-		event = "LspAttach",
-    dependencies = {
-        {"SmiteshP/nvim-navic",     lsp = {
-        auto_attach = true,
-    },event = "LspAttach"},
+    {
+        "folke/snacks.nvim",
+        event = "UiEnter",
+        ---@type snacks.Config
+        opts = {
+            picker = {enabled = true}
+        }
     },
+    {
+        "folke/flash.nvim",
+        event = "User FilePost",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
 
-
-opts ={},
-}
+            {"/", mode = {"n", "v"}, function()
+                    require("flash").toggle()
+                end, desc = "Toggle Flash Search"}
+        }
+    },
+    {
+        "SmiteshP/nvim-navic",
+        opts = {
+            lsp = {
+                auto_attach = true
+            }
+        },
+        event = "LspAttach",
+        dependencies = {
+            "LunarVim/breadcrumbs.nvim"
+        }
+    }
 }
