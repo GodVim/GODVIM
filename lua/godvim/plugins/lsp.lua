e1faf22af0c49691045b52bc88cad4ebba35f3f5
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-12 18:33:11 -0700

    Update lsp.lua

--- lua/godvim/plugins/lsp.lua
+++ lua/godvim/plugins/lsp.lua
@@ -1,94 +1,118 @@
 return {
-    "AstroNvim/astrolsp",
-    event = "User FilePost",
+  {
+    "neovim/nvim-lspconfig",
     dependencies = {
       "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "tsserver",
-        "jsonls",
-        "gopls",
-        "jdtls",
-        "yamlls",
-        "tailwindcss",
-        "marksman",
-        "bashls",
-        -- formatters/linters via mason too (if needed)
-        "biome",
-        "stylua",
-        "prettier",
-        "golangci-lint",
-        "markdownlint",
-        "luacheck",
-        "yamllint",
-        "taplo",
-      },
-      formatting = {
-        format_on_save = true,
-        disabled = { "lua_ls" }, -- use stylua via conform instead
-      },
-      linting = {
-        enabled = true,
-        linters_by_ft = {
-          javascript = { "biome" },
-          typescript = { "biome" },
-          json = { "biome" },
-          lua = { "luacheck" },
-          markdown = { "markdownlint" },
-          go = { "golangci_lint" },
-          yaml = { "yamllint" },
-          toml = { "taplo" },
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        opts = {
+          handlers = {
+            function(server)
+              require("astrolsp").lsp_setup(server)
+            end,
+          },
         },
+        config = function(_, opts)
+          -- Register servers AstroLSP manages before Mason setup
+          require("astrolsp.mason-lspconfig").register_servers()
+          require("mason-lspconfig").setup(opts)
+        end,
       },
-      setup = function()
-        -- This function is called internally by astrolsp after LSP setup
-
-        -- Setup conform.nvim for formatting, controlled by astrolsp
-        local conform = require("conform")
-        conform.setup({
-          format_on_save = {
-            timeout_ms = 1000,
-            lsp_fallback = true,
+      {
+        "AstroNvim/astrolsp",
+        opts = {
+          ensure_installed = {
+            "lua_ls",
+            "tsserver",
+            "jsonls",
+            "gopls",
+            "jdtls",
+            "yamlls",
+            "tailwindcss",
+            "marksman",
+            "bashls",
+            -- add formatters and linters you want mason to install
+            "biome",
+            "stylua",
+            "prettier",
+            "golangci-lint",
+            "markdownlint",
+            "luacheck",
+            "yamllint",
+            "taplo",
           },
-          formatters_by_ft = {
-            javascript = { "biome" },
-            typescript = { "biome" },
-            json = { "biome" },
-            markdown = { "prettier" },
-            lua = { "stylua" },
-            go = { "goimports", "gofmt" },
-            yaml = { "prettier" },
-            toml = { "taplo" },
+          formatting = {
+            format_on_save = true,
+            disabled = { "lua_ls" }, -- use stylua via conform instead
           },
-          formatters = {
-            biome = {
-              command = "biome",
-              args = { "format", "--stdin-file-path", "$FILENAME" },
-              stdin = true,
+          linting = {
+            enabled = true,
+            linters_by_ft = {
+              javascript = { "biome" },
+              typescript = { "biome" },
+              json = { "biome" },
+              lua = { "luacheck" },
+              markdown = { "markdownlint" },
+              go = { "golangci_lint" },
+              yaml = { "yamllint" },
+              toml = { "taplo" },
             },
           },
-        })
+          setup = function()
+            -- Setup conform.nvim formatting
+            local conform = require("conform")
+            conform.setup({
+              format_on_save = {
+                timeout_ms = 1000,
+                lsp_fallback = true,
+              },
+              formatters_by_ft = {
+                javascript = { "biome" },
+                typescript = { "biome" },
+                json = { "biome" },
+                markdown = { "prettier" },
+                lua = { "stylua" },
+                go = { "goimports", "gofmt" },
+                yaml = { "prettier" },
+                toml = { "taplo" },
+              },
+              formatters = {
+                biome = {
+                  command = "biome",
+                  args = { "format", "--stdin-file-path", "$FILENAME" },
+                  stdin = true,
+                },
+              },
+            })
 
-        -- Setup nvim-lint, controlled internally by astrolsp
-        local lint = require("lint")
-        lint.linters_by_ft = {
-          javascript = { "biome" },
-          typescript = { "biome" },
-          json = { "biome" },
-          lua = { "luacheck" },
-          markdown = { "markdownlint" },
-          go = { "golangci_lint" },
-          yaml = { "yamllint" },
-          toml = { "taplo" },
-        }
-        vim.api.nvim_create_autocmd("BufWritePost", {
-          callback = function()
-            lint.try_lint()
+            -- Setup nvim-lint
+            local lint = require("lint")
+            lint.linters_by_ft = {
+              javascript = { "biome" },
+              typescript = { "biome" },
+              json = { "biome" },
+              lua = { "luacheck" },
+              markdown = { "markdownlint" },
+              go = { "golangci_lint" },
+              yaml = { "yamllint" },
+              toml = { "taplo" },
+            }
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              callback = function()
+                lint.try_lint()
+              end,
+            })
           end,
-        })
-      end,
+        },
+      },
     },
-  }
+    config = function()
+      -- Setup all servers AstroLSP manages after Mason and mason-lspconfig
+      local servers = require("astrolsp").config.servers
+      for _, server in ipairs(servers) do
+        require("astrolsp").lsp_setup(server)
+      end
+    end,
+  },
+}
