eb5d1bb18e19a0268c5376621260a1d66efe28a7
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-13 16:44:10 -0700

    Update editor.lua

--- lua/godvim/plugins/editor.lua
+++ lua/godvim/plugins/editor.lua
@@ -13,44 +13,35 @@ return {
         event = "UiEnter",
         ---@type snacks.Config
         opts = {
-            picker = { enabled = true },
+            picker = {enabled = true}
         }
     },
-
     {
         "is0n/jaq-nvim",
         opts = {
-  cmds = {
-    -- Uses vim commands
-    internal = {
-      lua = "luafile %",
-      vim = "source %"
+            cmds = {
+                external = {
+                    markdown = "glow %",
+                    python = "python3 %",
+                    go = "go run %",
+                    sh = "sh %"
+                }
+            }
+        }
     },
-
-    -- Uses shell commands
-    external = {
-      markdown = "glow %",
-      python   = "python3 %",
-      go       = "go run %",
-      sh       = "sh %"
+    {
+        "hedyhli/outline.nvim",
+        keys = {{"<leader>cs", "<cmd>Outline<cr>", desc = "Toggle Outline"}},
+        cmd = "Outline",
+        opts = {
+            outline_window = {
+                position = "left",
+                width = 10
+            },
+            keymaps = {
+                up_and_jump = "<up>",
+                down_and_jump = "<down>"
+            }
+        }
     }
-  },
-
-}
-    },   
-  {
-    "hedyhli/outline.nvim",
-    keys = { { "<leader>cs", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
-    cmd = "Outline",
-    opts = {
-      outline_window = {
-        position = "left",
-        width = 10
-      },
-      keymaps = {
-        up_and_jump = "<up>",
-        down_and_jump = "<down>",
-      },
-    }
-  }
 }
