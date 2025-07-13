ab2a6b0ca6373fc77333f8583eb4991d26b9ff7a
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-13 14:53:48 -0700

    Update keybinds.lua

--- lua/godvim/plugins/keybinds.lua
+++ lua/godvim/plugins/keybinds.lua
@@ -1,46 +1,54 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		preset = "helix",
-		defaults = {},
-		spec = {
-			----- Groups -----
-			{ "<leader>c", group = "Code" },
-			{ "<leader>b", group = "Buffers" },
-			{ "<leader>s", group = "Search" },
-			{ "<leader>d", group = "Debug" },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			preset = "helix",
+			defaults = {},
+			spec = {
+				----- Groups -----
+				{ "<leader>c", group = "Code" },
+				{ "<leader>b", group = "Buffers" },
+				{ "<leader>s", group = "Search" },
+				{ "<leader>d", group = "Debug" },
 
-			----- Buffers -----
-			{ "<leader>bd", "<cmd>bdelete!<cr>", desc = "Close buffer" },
-			{ "<leader>bn", "<cmd>enew<cr>", desc = "New buffer" },
-			{ "<leader>bb", "<cmd>lua Snacks.picker.buffers()<cr>", desc = "Switch buffer" },
+				----- Buffers -----
+				{ "<leader>bd", "<cmd>bdelete!<cr>", desc = "Close buffer" },
+				{ "<leader>bn", "<cmd>enew<cr>", desc = "New buffer" },
+				{ "<leader>bb", "<cmd>lua Snacks.picker.buffers()<cr>", desc = "Switch buffer" },
 
-			----- Telescope -----
-			{ "<leader>sc", "<cmd>Themify<cr>", desc = "Themes" },
-			{ "<leader>sf", "<cmd>lua Snacks.picker.files()<cr>", desc = "Files" },
-			{ "<leader>sg", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Grep" },
-			{ "<leader>sb", "<cmd>lua Snacks.picker.buffers()<cr>", desc = "Buffers" },
-			{ "<leader>sh", "<cmd>lua Snacks.picker.help()<cr>", desc = "Help" },
-			{ "<leader>sm", "<cmd>lua Snacks.picker.marks()<cr>", desc = "Marks" },
-			{ "<leader>st", "<cmd>lua require('nvchad.themes').open()<cr>", desc = "Themes" },
+				----- Telescope -----
+				{ "<leader>sc", "<cmd>Themify<cr>", desc = "Themes" },
+				{ "<leader>sf", "<cmd>lua Snacks.picker.files()<cr>", desc = "Files" },
+				{ "<leader>sg", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Grep" },
+				{ "<leader>sb", "<cmd>lua Snacks.picker.buffers()<cr>", desc = "Buffers" },
+				{ "<leader>sh", "<cmd>lua Snacks.picker.help()<cr>", desc = "Help" },
+				{ "<leader>sm", "<cmd>lua Snacks.picker.marks()<cr>", desc = "Marks" },
+				{ "<leader>st", "<cmd>lua require('nvchad.themes').open()<cr>", desc = "Themes" },
+	
+				----- Code -----
+				{
+					"<leader>ca",
+					"<cmd>lua require('tiny-code-action').code_action()<cr>",
+					desc = "Code Action",
+				},
+				{ "<leader>cr", vim.lsp.buf.rename, desc = "Rename" },
 
-			----- Code -----
-			{
-				"<leader>ca",
-				"<cmd>lua require('tiny-code-action').code_action()<cr>",
-				desc = "Code Action",
+				----- random stuff -----
+				{ "<leader>,", "<cmd>lua Snacks.picker.buffers()<cr>", desc = "Buffers" },
+				{ "<leader>/", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Grep" },
+				{ "<leader>:", "<cmd>lua Snacks.picker.command_history()<cr>", desc = "Command History" },
+
+				{ "<leader>t", "<cmd>lua Snacks.terminal.toggle()<cr>", desc = "Terminal" },
+				{ "<leader>e", "<cmd>lua require('mini.files').open()<cr>", desc = "File Explorer" },
+				{ "<leader>l", "<cmd>Lazy<cr>", desc = "Lazy" },
 			},
-			{ "<leader>cr", vim.lsp.buf.rename, desc = "Rename" },
-
-			----- random stuff -----
-			{ "<leader>,", "<cmd>lua Snacks.picker.buffers()<cr>", desc = "Buffers" },
-			{ "<leader>/", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Grep" },
-			{ "<leader>:", "<cmd>lua Snacks.picker.command_history()<cr>", desc = "Command History" },
-
-			{ "<leader>t", "<cmd>lua Snacks.terminal.toggle()<cr>", desc = "Terminal" },
-			{ "<leader>e", "<cmd>lua require('mini.files').open()<cr>", desc = "File Explorer" },
-			{ "<leader>l", "<cmd>Lazy<cr>", desc = "Lazy" },
 		},
 	},
+	{
+		"nvim-zh/better-escape.vim",
+		opts = {},
+		event = "CursorMoved"
+	},
 }
+	
