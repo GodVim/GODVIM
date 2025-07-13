62ca6ad869a6f52614a6a5214fc9a641e5328492
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-13 16:30:08 -0700

    Update editor.lua

--- lua/godvim/plugins/editor.lua
+++ lua/godvim/plugins/editor.lua
@@ -16,25 +16,6 @@ return {
             picker = { enabled = true },
         }
     },
-{ -- This plugin
-  "Coding4Hours/compiler.nvim",
-  cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
-  dependencies = { "stevearc/overseer.nvim" },
-  opts = {},
-},
-{ -- The task runner we use
-  "stevearc/overseer.nvim",
-  commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
-  cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-  opts = {
-    task_list = {
-      direction = "bottom",
-      min_height = 25,
-      max_height = 25,
-      default_detail = 1
-    },
-  },
-},
     
   {
     "hedyhli/outline.nvim",
