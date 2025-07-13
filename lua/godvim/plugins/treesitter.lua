90bd2447004d0b504c574cb7544e43806d92fb0c
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-13 15:01:48 -0700

    Update treesitter.lua

--- lua/godvim/plugins/treesitter.lua
+++ lua/godvim/plugins/treesitter.lua
@@ -9,8 +9,10 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		lazy = true,
 		build = ":TSUpdate",
-		opts = {
+		config = function()
+		    require('nvim-treesitter.configs').setup({
 			sync_install = true,
+			auto_install = true,
 			highlight = {
 				enable = true,
 			},
@@ -29,7 +31,8 @@ return {
 				"gitignore",
 				"java",
 			},
-		},
+		    })
+		end,
 	},
 
 	{
