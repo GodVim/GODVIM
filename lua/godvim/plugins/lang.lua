5f8d4773695b863702bb4719b82f3ab09e555e64
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-13 20:55:39 -0700

    Update lang.lua

--- lua/godvim/plugins/lang.lua
+++ lua/godvim/plugins/lang.lua
@@ -1,27 +1,4 @@
 return {
-{
-  "ray-x/go.nvim",
-    event = "User FilePost",
-  dependencies = {
-    "ray-x/guihua.lua",
-  },
-  opts = {},
-  config = function(lp, opts)
-    require("go").setup(opts)
-    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-      require('go.format').goimports()
-      end,
-      group = format_sync_grp,
-    })
-  end,
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-},
-  
   -- astrolsp
   {
     "AstroNvim/astrolsp",
