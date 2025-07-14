af7e2bb8675e32e96a06b5d99049ecf5e79d1fa7
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-14 15:50:12 -0700

    Update keymaps.lua

--- lua/godvim/config/keymaps.lua
+++ lua/godvim/config/keymaps.lua
@@ -55,7 +55,7 @@ map("n", "<leader>st", function() require("nvchad.themes").open() end, { desc = 
 -- Code
 map("n", "<leader>cg", "<cmd>Jaq<cr>", { desc = "Run", silent = true })
 map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
-map("n", "<leader>cf", vim.lsp.buf.format, { desc = "Rename" })
+map("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
 
 -- Random stuff
 map("n", "<leader>,", function() Snacks.picker.buffers() end, { desc = "Buffers" })
