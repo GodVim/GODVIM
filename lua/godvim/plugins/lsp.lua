344cb87ecb324490e1d493da32c003388ff336ea
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-13 20:44:16 -0700

    Update lsp.lua

--- lua/godvim/plugins/lsp.lua
+++ lua/godvim/plugins/lsp.lua
@@ -1,48 +1,5 @@
 return {
-  -- astrolsp
-  {
-    "AstroNvim/astrolsp",
-    event = "User FilePost",
-    opts = {
-      features = {
-        autoformat = true,
-        codelens = true,
-        inlay_hints = false,
-        semantic_tokens = true,
-      },
-      ensure_installed = {
-        "lua_ls", "jsonls", "gopls", "yamlls", "tailwindcss",
-        "marksman", "bashls", "biome", "stylua", "prettier", "golangci-lint",
-        "markdownlint", "luacheck", "yamllint", "taplo",
-      },
-      formatting = {
-        format_on_save = true,
-        formatters_by_ft = {
-          javascript = { "biome" },
-          typescript = { "biome" },
-          json = { "biome" },
-          markdown = { "prettier" },
-          lua = { "stylua" },
-          go = { "goimports", "gofmt" },
-          yaml = { "prettier" },
-          toml = { "taplo" },
-        },
-      },
-      linting = {
-        enabled = true,
-        linters_by_ft = {
-          javascript = { "biome" },
-          typescript = { "biome" },
-          json = { "biome" },
-          lua = { "luacheck" },
-          markdown = { "markdownlint" },
-          go = { "golangci_lint" },
-          yaml = { "yamllint" },
-          toml = { "taplo" },
-        },
-      },
-    },
-  },
+
 	-- garbage
 	{
   "zeioth/garbage-day.nvim",
@@ -121,28 +78,4 @@ return {
       },
     }
   },
-
-
-
-  
-	-- Java Support
-	{
-		"nvim-java/nvim-java",
-		ft = "java",
-		config = function()
-			require("java").setup({
-				jdk = {
-					auto_install = false,
-				},
-			})
-			require("lspconfig").jdtls.setup({})
-		end,
-	},
-
-	-- Code Actions
-	{
-		"rachartier/tiny-code-action.nvim",
-		event = "LspAttach",
-		opts = {},
-	},
 }
