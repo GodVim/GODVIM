c285e566b57ae3cfd525dcd0f55c23b276673fb9
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-14 16:17:41 -0700

    Update keymaps.lua

--- lua/godvim/keymaps.lua
+++ lua/godvim/keymaps.lua
@@ -75,19 +75,5 @@ map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
 map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 
 
-map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
-
-map("n", "<tab>", function()
-  require("nvchad.tabufline").next()
-end, { desc = "buffer goto next" })
-
-map("n", "<S-tab>", function()
-  require("nvchad.tabufline").prev()
-end, { desc = "buffer goto prev" })
-
-map("n", "<leader>x", function()
-  require("nvchad.tabufline").close_buffer()
-end, { desc = "buffer close" })
-
 
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
