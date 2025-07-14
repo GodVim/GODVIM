e16eb4429867a831e05e38d3731267a1fb53a9db
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-14 15:57:41 -0700

    Update options.lua

--- lua/godvim/config/options.lua
+++ lua/godvim/config/options.lua
@@ -25,8 +25,9 @@ if vim.g.neovide then
 end
 
 opt.updatetime = 200
-opt.clipboard:append("unnamedplus")
-
+o.clipboard = "unnamedplus"
+o.cursorline = true
+o.cursorlineopt = "number"
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append("<>[]hl")
