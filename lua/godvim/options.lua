9278baabfbdaaac065378abc8563515bb205f3e8
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-22 16:43:07 -0700

    Update options.lua

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -40,3 +40,17 @@ vim.diagnostic.config({
 
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_editorconfig = 1
+vim.g.loaded_gzip  = 1
+vim.g.loaded_man  = 1
+vim.g.loaded_matchit  = 1
+vim.g.loaded_matchparen  = 1
+vim.g.loaded_netrwPlugin  = 1
+vim.g.loaded_osc52  = 1
+vim.g.loaded_rplugin  = 1
+vim.g.loaded_shada  = 1
+vim.g.loaded_spellfile  = 1
+vim.g.loaded_tarPlugin  = 1
+vim.g.loaded_tohtml = 1
+vim.g.loaded_tutor  = 1
+vim.g.loaded_zipPlugin = 1
