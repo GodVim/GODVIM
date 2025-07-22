ff9471916b77888dc2a3b2b640704c10fb2277a3
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-22 16:37:56 -0700

    Update editor.lua

--- lua/godvim/plugins/editor.lua
+++ lua/godvim/plugins/editor.lua
@@ -10,18 +10,13 @@
     {
       "<leader>-",
       mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
+      "<cmd>Yazi toggle<cr>",
       desc = "Open yazi at the current file",
     },
     {
       "<leader>cw",
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
     },
   },
   opts = {
