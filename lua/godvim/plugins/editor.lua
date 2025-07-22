6a82576758b7bac612e9928ab3e7f4e35282f83a
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-22 16:36:26 -0700

    Update editor.lua

--- lua/godvim/plugins/editor.lua
+++ lua/godvim/plugins/editor.lua
@@ -1,4 +1,41 @@
                       return {
+	---@type LazySpec
+{
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+  init = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
+	
     {
         "folke/snacks.nvim",
         event = "User FilePost",
