e717c59f030348a69bd11dffe476db3cc64f8b75
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-24 09:57:51 -0700

    Update windsurf.lua

--- lua/godvim/plugins/extras/ai/windsurf.lua
+++ lua/godvim/plugins/extras/ai/windsurf.lua
@@ -1,20 +1,21 @@
 return {
+  -- Windsurf
   {
-  "Exafunction/codeium.nvim",
-  event = "User FilePost",
-  lazy = true,
-  build = ":Codeium Auth",
-  opts = {
-    enable_cmp_source = true,
-    virtual_text = {
-      enabled = false,
-      key_bindings = {
-        next = "<M-]>",
-        prev = "<M-[>",
+    "Exafunction/codeium.nvim",
+    event = "User FilePost",
+    lazy = true,
+    build = ":Codeium Auth",
+    opts = {
+      enable_cmp_source = true,
+      virtual_text = {
+        enabled = false,
+        key_bindings = {
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
       },
     },
   },
-},
   
   -- Blink.cmp
   {
@@ -37,5 +38,4 @@ return {
       },
     },
   }
-
 }
