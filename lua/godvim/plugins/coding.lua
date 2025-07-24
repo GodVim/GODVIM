07bb0c3af66bc077d928da74e57acbd9799a9319
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-23 21:24:00 -0700

    Update coding.lua

--- lua/godvim/plugins/coding.lua
+++ lua/godvim/plugins/coding.lua
@@ -1,123 +1,85 @@
 return {
-	-- Pairs
-	{
-		"echasnovski/mini.pairs",
-		event = "InsertEnter",
-		version = false,
-		opts = {},
-	},
-	-- Commenting
-	{
-		"echasnovski/mini.comment",
-		event = "User FilePost",
-		version = false,
-		opts = {},
-	},
+  -- Autopairs
+  { "echasnovski/mini.pairs", event = "InsertEnter", opts = {} },
 
-	-- Surround
-	{
-		"echasnovski/mini.surround",
-		event = "InsertEnter",
-		version = false,
-		opts = {},
-	},
+  -- Commenting
+  { "echasnovski/mini.comment", event = "User FilePost", opts = {} },
 
-	{
-		"echasnovski/mini.snippets",
-		event = "InsertEnter",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
-		version = false,
-		config = function()
-			local gen_loader = require("mini.snippets").gen_loader
-			require("mini.snippets").setup({
-				snippets = {
-					gen_loader.from_file("~/.config/nvim/snippets/global.json"),
+  -- Surround
+  { "echasnovski/mini.surround", event = "InsertEnter", opts = {} },
 
-					gen_loader.from_lang(),
-				},
-			})
-		end,
-	},
-	{
-  "Exafunction/codeium.nvim",
-  cmd = "Codeium",
-  event = "InsertEnter",
-  build = ":Codeium Auth",
-  opts = {
-    enable_cmp_source = true,
-    virtual_text = {
-      enabled = false,
-      key_bindings = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
+  -- Mini Snippets
+  {
+    "echasnovski/mini.snippets",
+    event = "InsertEnter",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      local loader = require("mini.snippets").gen_loader
+      require("mini.snippets").setup({
+        snippets = {
+          loader.from_file("~/.config/nvim/snippets/global.json"),
+          loader.from_lang(),
+        },
+      })
+    end,
+  },
+
+  -- Codeium
+  {
+    "Exafunction/codeium.nvim",
+    event = "InsertEnter",
+    build = ":Codeium Auth",
+    opts = {
+      enable_cmp_source = true,
+      virtual_text = {
+        enabled = false,
+        key_bindings = {
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
       },
     },
   },
-},
 
-	
-	{
-		"Saghen/blink.cmp",
-		dependencies = {
-			{
-				"zbirenbaum/copilot.lua",
-				build = ":Copilot auth",
-				cmd = "Copilot",
-				opts = {
-					filetypes = {
-						markdown = true,
-						help = true,
-					},
-				},
-			},
-			"codeium.nvim", "saghen/blink.compat",
-			"mikavilpas/blink-ripgrep.nvim",
-			"giuxtaposition/blink-cmp-copilot",
-		},
-		event = {
-			"User FilePost",
-			"CmdlineChanged",
-		},
-	version = "*",
-
-		opts = {
-			keymap = { preset = "super-tab" },
-			snippets = { preset = "mini_snippets" },
-			cmdline = {
-				completion = { menu = { auto_show = true }, ghost_text = { enabled = true } },
-			},
-			completion = {
-				list = {
-					selection = { auto_insert = false },
-				},
-			},
-			sources = {
-
-				default = { "codeium", "snippets", "lsp", "ripgrep" },
-				providers = {
-					copilot = {
-						name = "copilot",
-						module = "blink-cmp-copilot",
-						score_offset = 100,
-						async = true,
-					},
-        codeium = { name = 'Codeium', module = 'codeium.blink', async = true },
-
-					ripgrep = {
-						module = "blink-ripgrep",
-						name = "Ripgrep",
-						score_offset = -4,
-						opts = {
-							prefix_mix_len = 3,
-							context_size = 5,
-							max_filesize = "1M",
-						},
-					},
-				},
-			},
-		},
-	},
+  -- Blink.cmp
+  {
+    "Saghen/blink.cmp",
+    event = { "User FilePost", "CmdlineChanged" },
+    version = "*",
+    dependencies = {
+      "codeium.nvim",
+--      "saghen/blink.compat",
+      "mikavilpas/blink-ripgrep.nvim",
+    },
+    opts = {
+      keymap = { preset = "super-tab" },
+      snippets = { preset = "mini_snippets" },
+      cmdline = {
+        completion = { menu = { auto_show = true }, ghost_text = { enabled = true } },
+      },
+      completion = {
+        list = { selection = { auto_insert = false } },
+      },
+      sources = {
+        default = { "codeium", "snippets", "lsp", "ripgrep" },
+        providers = {
+          codeium = {
+            name = "Codeium",
+            module = "codeium.blink",
+            async = true,
+          },
+          ripgrep = {
+            name = "Ripgrep",
+            module = "blink-ripgrep",
+            score_offset = -4,
+            opts = {
+              prefix_mix_len = 3,
+              context_size = 5,
+              max_filesize = "1M",
+            },
+          },
+        },
+      },
+    },
+  },
 }
