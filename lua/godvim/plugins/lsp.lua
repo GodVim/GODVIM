6b80ca70f8c980c954470d1201760eb982fb2531
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-24 12:21:47 -0700

    Update lsp.lua

--- lua/godvim/plugins/lsp.lua
+++ lua/godvim/plugins/lsp.lua
@@ -89,31 +89,6 @@ return {
   },
 },
 
--- nvim-lint (linter)
-{
-  "mfussenegger/nvim-lint",
-  event = "BufEnter",
-  config = function()
-    local lint = require("lint")
-    lint.linters_by_ft = {
-      javascript = { "biome" },
-      typescript = { "biome" },
-      json = { "biome" },
-      lua = { "luacheck" },
-      markdown = { "markdownlint" },
-      go = { "golangci_lint" },
-      yaml = { "yamllint" },
-      toml = { "taplo" },
-    }
-
-   vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-  end,
-},
   
 	-- Java Support
 	{
