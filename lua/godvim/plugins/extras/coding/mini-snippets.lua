f0a24b049291c4e502317058e006defbf6217954
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-24 09:56:00 -0700

    Update mini-snippets.lua

--- lua/godvim/plugins/extras/coding/mini-snippets.lua
+++ lua/godvim/plugins/extras/coding/mini-snippets.lua
@@ -1,14 +1,25 @@
 return {
-  "echasnovski/mini.snippets",
-  event = "InsertEnter",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  config = function()
-    local loader = require("mini.snippets").gen_loader
-    require("mini.snippets").setup({
-      snippets = {
-        loader.from_file("~/.config/nvim/snippets/global.json"),
-        loader.from_lang(),
+  {
+    "echasnovski/mini.snippets",
+    lazy = true,
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      local loader = require("mini.snippets").gen_loader
+      require("mini.snippets").setup({
+        snippets = {
+          loader.from_file("~/.config/nvim/snippets/global.json"),
+          loader.from_lang(),
+        },
+      })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      snippets = { preset = "mini_snippets" },
+      sources = {
+        default = { "snippets" },
       },
-    })
-  end,
+    }
+  }
 }
