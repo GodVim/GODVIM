8af1e1896b0e5240bc26287f83c59a831918a2dc
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-24 18:55:15 -0700

    Update ui.lua

--- lua/godvim/plugins/ui.lua
+++ lua/godvim/plugins/ui.lua
@@ -56,6 +56,22 @@ return {
 
 
       alpha.setup(dashboard.config)
+				      vim.api.nvim_create_autocmd("User", {
+        once = true,
+        pattern = "LazyVimStarted",
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          dashboard.section.footer.val = "⚡ Neovim loaded "
+            .. stats.loaded
+            .. "/"
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
     end,
   },
 },
