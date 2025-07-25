1ac6b8abb7134d37a6a774e07d4a789f0fea99d8
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-24 18:23:11 -0700

    Update ui.lua

--- lua/godvim/plugins/ui.lua
+++ lua/godvim/plugins/ui.lua
@@ -10,53 +10,14 @@ return {
     },
   },
 
-  -- Statusline Tabline Lsp
   {
-    "nvchad/ui",
+    "ccxnu/rosebones",
     event = "VimEnter",
+    priority = 1000,
     config = function()
-      require("nvchad")
-      dofile(vim.g.base46_cache .. "defaults")
-      dofile(vim.g.base46_cache .. "statusline")
-      --dofile(vim.g.base46_cache .. "tbline")
-      dofile(vim.g.base46_cache .. "syntax")
-      dofile(vim.g.base46_cache .. "blink")
-
-      dofile(vim.g.base46_cache .. "lsp")
-      dofile(vim.g.base46_cache .. "treesitter")
-      dofile(vim.g.base46_cache .. "colors")
+      require("rosebones").setup({})
+      vim.cmd.colorscheme('rosebones')
+      return {}
     end,
-  },
-
-  {
-    event="VeryLazy",
-  "tiagovla/scope.nvim",
-    opts = {},
-  config = true
-  },
-  
-  -- Colorscheme
-  {
-    "nvchad/base46",
-    lazy = true,
-    build = function()
-      require("base46").load_all_highlights()
-    end,
-  },
-  
-  -- Icons
-  {
-  "nvim-tree/nvim-web-devicons",
-  event = "User FilePost",
-  opts = function()
-    dofile(vim.g.base46_cache .. "devicons")
-    return { override = require "nvchad.icons.devicons" }
-  end,
-  },
-
-  -- Ui Library
-  {
-    "nvchad/volt",
-    lazy = true,
   }
 }
