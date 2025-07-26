55d2fe176c9856f1ee77942bc7704e7252b8f873
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-25 17:16:58 -0700

    Update keymaps.lua

--- lua/godvim/keymaps.lua
+++ lua/godvim/keymaps.lua
@@ -8,10 +8,10 @@ map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
 
 -- Moving lines and preserving indentation
-map('n', '<C-j>', "<cmd>move .+1<cr>==")
-map('n', '<C-k>', "<cmd>move .-2<cr>==")
-map('x', '<C-j>', "<esc><cmd>'<,'>move '>+1<cr>gv=gv")
-map('x', '<C-k>', "<esc><cmd>'<,'>move '<-2<cr>gv=gv")
+map('n', '<A-j>', "<cmd>move .+1<cr>==")
+map('n', '<A-k>', "<cmd>move .-2<cr>==")
+map('x', '<A-j>', "<esc><cmd>'<,'>move '>+1<cr>gv=gv")
+map('x', '<A-k>', "<esc><cmd>'<,'>move '<-2<cr>gv=gv")
 
 
 -- change font
