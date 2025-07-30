9df2c107b04dc207aea9641c956cc7f4a475deeb
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-30 15:25:57 -0700

    Update mini-tabline.lua

--- lua/godvim/extras/ui/mini-tabline.lua
+++ lua/godvim/extras/ui/mini-tabline.lua
@@ -1,7 +1,13 @@
 return {
-	"echasnovski/mini.tabline",
-  desc = "A minimal and fast tabline from mini.nvim",
-	event = "BufReadPost",
-	version = false,
-  opts = {},
+  {
+    "echasnovski/mini.tabline",
+    desc = "A minimal and fast tabline from mini.nvim",
+    event = "BufReadPost",
+    version = false,
+    opts = {},
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false
+  },
 }
