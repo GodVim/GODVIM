159e6addb7eaa8643879353186808de5b8836862
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-31 13:32:46 -0700

    Create oil.lua

---
+++ lua/godvim/extras/editor/oil.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    cmd = { "Oil" },
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "Oil Buffer" },
+    },
+    opts = {},
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+}
