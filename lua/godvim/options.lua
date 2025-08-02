a7f563ee6c010aa14c1abd7995c831a2a965a18a
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-08-02 10:12:43 -0700

    Update options.lua

--- lua/godvim/options.lua
+++ lua/godvim/options.lua
@@ -4,11 +4,16 @@ local o = vim.o
 
 opt.shortmess:append "sI"
 o.showtabline = 2
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
 o.cmdheight = 0
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.laststatus = 3
+
+
 opt.smoothscroll = true
 opt.smartindent = true
 opt.ignorecase = true
